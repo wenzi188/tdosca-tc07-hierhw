ea88625b0a7adb2360e1574e6a50c3b9e0f4d7d1
Reinhardt <reinhardt@teasy.at>
2021-12-07 12:27:20 +0100

    Changes to LGPL+

--- input-sources/src/main/java/de/tdosca/src/MatchLGPL.java
+++ input-sources/src/main/java/de/tdosca/src/MatchLGPL.java
@@ -1,11 +1,9 @@
-/*
-Copyright (C) 2021 Reinhardt Wenzina
+/* ) 2021 Reinhardt Wenzina
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3 of the License, or (at your option) any later version.
-*/
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version. */
 
 package de.tdosca.src;
 

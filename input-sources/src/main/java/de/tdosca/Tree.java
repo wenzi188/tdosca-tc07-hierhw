968d91eb2259471d298b3cdf57a19870a8a9d6e6
Reinhardt <reinhardt@teasy.at>
2021-12-07 09:35:58 +0100

    Initial Commit

---
+++ input-sources/src/main/java/de/tdosca/Tree.java
@@ -0,0 +1,4 @@
+package de.tdosca;
+
+public class Tree {
+}
---
+++ input-sources/src/main/java/tc07/Main.java
@@ -0,0 +1,7 @@
+package tc07;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("de.tdosca.tc07.Main: 'hello'.");
+    }
+}
